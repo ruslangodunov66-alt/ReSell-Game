652793388eccb4a5617edece1b16147bd5ee1f8b
ruslangodunov66-alt <ruslangodunov66@gmail.com>
2026-04-29 18:00:21 +0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 ﻿import os
 
-BOT_TOKEN = os.environ.get("8747685010:AAH8bN3x0fihSvUzVitijYQLHXeHFhIV5w4")
+# Если переменная окружения не задана — используем токен из кода
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8747685010:AAH8bN3x0fihSvUzVitijYQLHXeHFhIV5w4")
 ADMIN_IDS = [1475910449]
